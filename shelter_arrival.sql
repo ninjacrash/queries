37f6a33b7d9a9364e8f0844eed6f719e41ca34dc
clesiemo3 <clesiemo3@gmail.com>
2016-10-22 03:08:05 -0500

    shelter arrival

---
+++ shelter_arrival.sql
@@ -0,0 +1,3 @@
+--insert into public.events (user_id,actor_id,event_type,extra) values ('gufran@aol.com',1,'entered_shelter','{"message": "So happy to find a place to sleep.", "latitude": 37.6265365, "longitude": -91.2324329}')
+--insert into public.user_shelter (user_id,shelter_id) values ('gufran@aol.com',1)
+
